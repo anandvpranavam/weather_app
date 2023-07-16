358c57bdd20c729220243dee0bb4a15c0340b02f
Anand Venugopalapanicker <anandvpranavam@gmail.com>
2023-07-16 14:20:38 -0400

    added main.innerHTM='' for refresh option each time searching for new place weather detail

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ async function getGeoData(cityName) {
     }
 }
 
-
 function displayWeather(data) {
 
     // get template
@@ -21,29 +20,23 @@ function displayWeather(data) {
     const cloneSummaryWeather = mainTemplate.content.cloneNode(true)
     // replace cloned content with actual
     console.log(data)
-    cloneSummaryWeather.querySelector(".mainWeather").innerText = "Weather: " + data.weather[0].main
-    cloneSummaryWeather.querySelector(".mainWeatherDesc").innerText = "Description: " + data.weather[0].description
-    cloneSummaryWeather.querySelector(".weatherIcon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-    cloneSummaryWeather.querySelector(".mainTemp").innerText = (data.main.temp-273.15).toFixed(1)+"°C"
+    
+    cloneSummaryWeather.querySelector(".mainWeather").innerText =  data.weather[0].main
+    // cloneSummaryWeather.querySelector(".mainWeatherDesc").innerText =  data.weather[0].description
+    cloneSummaryWeather.querySelector(".weatherIcon").src = (`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+    cloneSummaryWeather.querySelector(".mainTemp").innerText = (data.main.temp-273.15).toFixed(0)+"°C"
     cloneSummaryWeather.querySelector(".feelsLikeTemp").innerText = "Feels like: "+(data.main.feels_like-273.15).toFixed(2)+"°C"
     cloneSummaryWeather.querySelector(".humidity").innerText = "Humidity: "+(data.main.humidity).toFixed(0)+"%"
     cloneSummaryWeather.querySelector(".windSpeed").innerText = "Wind speed: "+(data.wind.speed).toFixed(2)+" m/s"
 
-
-
     // append replaced data to main
     const mainDiv = document.getElementById("main")
     mainDiv.appendChild(cloneSummaryWeather)
-
-
-
-
 }
-
-
 
 async function getWeatherData(cityName) {
     console.log("I called cityname")
+    main.innerHTML="";
     const geoData = await getGeoData(cityName)
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geoData.lat}&lon=${geoData.lon}&appid=2539dc11b80675feb8e8c2823e6546cf`)
     if (response.status === 200) {
@@ -56,6 +49,7 @@ async function getWeatherData(cityName) {
 async function handleSubmit(event) {
     event.preventDefault()
     // console.log("submit")
+
     const input = document.querySelector("#cityInput")
     const inputCityName = input.value
     const data = await getWeatherData(inputCityName)
@@ -65,7 +59,3 @@ async function handleSubmit(event) {
 
 const form = document.querySelector("#myForm")
 form.addEventListener("submit", handleSubmit)
-// console.log(form)
-
-
-// getWeatherData("London")
